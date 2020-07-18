8ae6416917418a72e04bcf03af0cb17b986d6a3d
SalmanKarim42 <salmankarimkhancs@outlook.com>
2020-07-18 11:21:15 +0500

    added readme and starater config

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize')
 const helmet = require('helmet')
 const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
+const cors = require('cors')
 
 // load env vars
 dotenv.config({ path: './config/config.env' })
@@ -48,6 +51,19 @@ app.use(helmet());
 
 //  Prevent XSS attacks 
 app.use(xss());
+
+//  Rate Limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes 
+    max: 100
+})
+app.use(limiter);
+
+//  Prevent http param polution
+app.use(hpp());
+
+//  Enable CORS
+app.use(cors());
 
 // Set Static folder 
 app.use(express.static(path.join(__dirname, 'public')))
